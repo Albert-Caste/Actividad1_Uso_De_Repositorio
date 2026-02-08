aeb52895ea434b6616d2a21199db191adc01d939
saly <acastellanosb@uqvirtual.edu.co>
2026-02-07 19:26:51 -0500

    Version 1

---
+++ src/Actividad1.java
@@ -0,0 +1,38 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Actividad1 {
+    public static void main(String [] args){
+
+        int numMaximo = 120;
+        String primos = listarPrimos(numMaximo);
+        System.out.println("Los primos menores que: " + numMaximo + " son "+ primos );
+
+    }
+
+    public static boolean validarPrimo(int num){
+        boolean primo = true;
+        int divisores = 0;
+        for (int i = 1; i <= num/2; i++){
+            if ( num % i == 0){
+                divisores ++;
+            }
+            if (divisores ==2){
+                primo =false;
+                break;
+            }
+        }
+        return primo;
+    }
+
+
+    public static String listarPrimos(int numMaximo){
+        String listaPrimos = "";
+        for (int i = 1; i <=numMaximo; i++){
+            if (validarPrimo(i)){
+                listaPrimos += i + " ";
+            }
+        }
+
+        return  listaPrimos;
+    }
+}
