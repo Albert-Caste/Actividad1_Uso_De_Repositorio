cddfc3e91574ffdcd8c8cc84287761d35151b5e0
saly <acastellanosb@uqvirtual.edu.co>
2026-03-29 20:43:04 -0500

    Cambio

--- Agenda/src/Main.java
+++ Agenda/src/Main.java
@@ -1,6 +1,8 @@
 import javax.swing.*;
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
@@ -17,10 +19,10 @@ public class Main {
                 "Marque alguna de estas opciones \n" +
                 "  \n" +
                 "  " +
-                "1. Registrar Contacto \n" +
+                "1. Buscar\n" +
                 "2. Crear grupo \n" +
-                "3. Crear Reunion \n" +
-                "4. Buscar  \n" +
+                "3. Registrar Contacto\n" +
+                "4. Crear Reunion \n" +
                 "5 \n" +
                 "\n" +
                 "\n" +
@@ -73,17 +75,113 @@ public class Main {
 
 
             switch (opcion){
-                case 1 : {
+                case 1 : { inputBuscar = JOptionPane.showInputDialog(menuBuscar);
+
+                    try{
+                        opcionBuscar = Integer.parseInt(inputBuscar);
+                    } catch (NumberFormatException e){
+                        JOptionPane.showMessageDialog(null,"Debe ingrear un número correspondiente a una opción");
+                        continue;
+                    }
+                    switch (opcionBuscar){
+                        case 1 : {
+                            String telefono = JOptionPane.showInputDialog("Ingrese el telefono del contacto a buscar");
+                            Contacto contacto = agenda1.buscarContacto(telefono);
+                            if (contacto==null){
+                                JOptionPane.showMessageDialog(null, "No encontrado");
+                                continue;
+                            }else {
+                                JOptionPane.showMessageDialog(null , "El contacto buscado es " + contacto.getNombre());
+                            }
+                            continue;
+
+
+                        }
+                        case 2 : {
+                            String idGrupo = JOptionPane.showInputDialog("Ingrese id  del grupo a buscar");
+                            int id = Integer.parseInt(idGrupo);
+
+                            Grupo grupo = agenda1.buscarGrupo(id);
+                            if (grupo==null){
+                                JOptionPane.showMessageDialog(null, "No encontrado");
+                            }else {
+                                JOptionPane.showMessageDialog(null , "El grupo buscado es " + grupo.getNombre());
+                                JOptionPane.showMessageDialog(null, grupo.toString());
+                            }
+                            continue;
+                        }
+
+                        case 3 : {
+                            //buscar contactos del mismo nombre
+                            String nombreABuscar = JOptionPane.showInputDialog("Ingrese id  del grupo a buscar");
+
+                            String listaIguales = agenda1.sacarListaNombresIguales(nombreABuscar);
+                            JOptionPane.showMessageDialog(null,listaIguales);
+
+                            continue;
+                        }
+
+                        case 4 : {
+                            //mostrarGrupoContacto
+                            String idGrupo = JOptionPane.showInputDialog("Ingrese Id del grupo");
+                            int idABuscar = Integer.parseInt(idGrupo);
+                            Grupo grupo = agenda1.buscarGrupo(idABuscar);
+                            JOptionPane.showMessageDialog(null, grupo.toString());
+
+
+                            continue;
+
+                        }
+
+                        case 7: continue;
+
+                        default: JOptionPane.showMessageDialog(null,"No marcó una opción valida");
+                        continue;
+
+
+
+                    }
+
 
                 }
 
                 case 2 : {
+                    String nombreDelGrupo =JOptionPane.showInputDialog("ingrese nombre del grupo");
+                    String idGrupo = JOptionPane.showInputDialog("Ingrese la id del grupo");
+                    int idNuevo = Integer.parseInt(idGrupo);
 
+                    String  mensajeGrupo = agenda1.crearGrupo(nombreDelGrupo,idNuevo);
+                    JOptionPane.showMessageDialog(null,mensajeGrupo);
+                    continue;
                 }
 
-                default:
+                case 3 : {
+                    String nombreContacto = JOptionPane.showInputDialog("Ingrese el nombre");
+                    String alias = JOptionPane.showInputDialog("Ingrese el alias");
+                    String direccion = JOptionPane.showInputDialog("Ingrese el direccion");
+                    String email = JOptionPane.showInputDialog("Ingrese el email");
+                    String telefono = JOptionPane.showInputDialog("Ingrese el telefono");
+                    String mensajeRegistro = agenda1.registrarContacto(nombreContacto, alias, direccion, email, telefono);
+                    JOptionPane.showMessageDialog(null, mensajeRegistro);
+
+                    String idGrupoARegistrar = JOptionPane.showInputDialog("Ingrese el id del grupo para agregar su contacto ");
+                    int id = Integer.parseInt(idGrupoARegistrar);
+                    String registro = agenda1.ingresarContactoAgrupo(telefono , id);
+                    JOptionPane.showMessageDialog(null, registro);
+
+
+
+
+
+                    continue;
+                }
+
+                case  8 : break;
+
+                default: break;
 
             }
+            break;
         }
 
 
